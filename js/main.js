9f07f4b298efe7885ae81f7864f2d9baa0762f26
jason-leduc <jasonleduc@jasons-mbp.gateway.2wire.net>
2015-03-11 22:23:38 -0400

    added proper javascript variable and btn move and show-hide functions

--- js/main.js
+++ js/main.js
@@ -3,3 +3,16 @@ var $diamond = $('.diamond');
 var $panel = $('.panel');
 var $circle = $('.circle');
 var $list = $('.list');
+
+$('.btn-show-hide').on('click', function (e) {
+	e.preventDefault();
+	// console.log('clicked-show-hide')
+	$box.toggleClass('js-show-hide');
+});
+
+$('.btn-move').on('click', function (e) {
+	e.preventDefault();
+	// console.log('clicked-move')
+	$diamond.toggleClass('js-move');
+});
+
