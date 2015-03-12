50bf8d9dd7b4ed69badd6a3db1c1f12b1a8ea5db
jason-leduc <jasonleduc@jasons-mbp.gateway.2wire.net>
2015-03-11 22:18:05 -0400

    added proper javascript variables

--- js/main.js
+++ js/main.js
@@ -1,23 +1,5 @@
-var btnShowHide = $('.btn-show-hide');
-var btnMove = $('.btn-move');
-var btnCollapseExpand = $('.btn-collapse-expand');
-
-$.btnShowHide.on('click', function () {
-  $box.toggleClass('js-box-show')
-});
-
-$.btnMove.on('click', function () {
-  $diamond.toggleClass('js-diamond-move')
-});
-
-$.btnCollapseExpand('click', function () {
-  $panel.toggleClass('js-panel-collapse')
-});
-
-$btnBounce.on('click', function () {
-  $circle.addClass('js-ball-bounce')
-});
-
-$circle.on('webkitAnimationEnd animationend', function () {
-  $circle.removeClass('js-ball-bounce')
-});
+var $box = $('.box');
+var $diamond = $('.diamond');
+var $panel = $('.panel');
+var $circle = $('.circle');
+var $list = $('.list');
